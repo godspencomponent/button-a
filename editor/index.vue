0fc48911d8d2ffa5122df814fa3e2aa500c5d4e4
panaru <aru.pana@ymm56.com>
2019-07-21 16:28:07 +0800

    普通按钮

---
+++ editor/index.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="component-editor">
+    <el-card class="box-card clearfix">
+     <el-form ref="form" label-width="80px" label-position='left' size='mini'>
+      <el-form-item label="文字:">
+        <el-input v-model="componentInfo.text" placeholder="请输入按钮文字"></el-input>
+      </el-form-item>
+      <el-form-item label="链接:">
+        <el-input v-model="componentInfo.url" placeholder="请输入链接"></el-input>
+      </el-form-item>
+      <el-form-item label="文字颜色:">
+        <el-color-picker v-model="componentInfo.color" show-alpha></el-color-picker>
+        <span style='vertical-align: top'>{{componentInfo.color}}</span>
+      </el-form-item>
+      <el-form-item label="背景色:">
+        <el-color-picker v-model="componentInfo.backgroundColor" show-alpha></el-color-picker>
+        <span style='vertical-align: top'>{{componentInfo.backgroundColor}}</span>
+      </el-form-item>
+      <el-form-item label="圆角:">
+        <el-input placeholder="请输入圆角大小" v-model.number="componentInfo.radius" type='number' min='0'>
+          <template slot="append">px</template>
+        </el-input>
+      </el-form-item>
+      <el-form-item label="边框:">
+        <el-input placeholder="请输入边框大小" v-model="componentInfo.border" min='0' type='number'>
+          <template slot="append">px</template>
+        </el-input>
+      </el-form-item>
+      <el-form-item label="边框颜色:">
+        <el-color-picker v-model="componentInfo.borderColor" show-alpha></el-color-picker>
+        <span style='vertical-align: top'>{{componentInfo.borderColor}}</span>
+      </el-form-item>
+    </el-form>
+    </el-card>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'maliangeditor',
+    props: {
+      componentInfo: { // 固定字段，收集所有属性值
+        type: [Object],
+        default () {
+          return {
+            text: '提交',
+            color: 'rgb(255, 255, 255)',
+            url: '',
+            radius: '3',
+            border: '0',
+            borderColor: '',
+            backgroundColor: 'rgb(2, 181, 136)'
+          }
+        }
+      }
+    },
+    data: function () {
+      return {
+      }
+    },
+    computed: {
+    },
+    watch: {
+      componentInfo: {
+        hanlder: function (v) {
+          console.log('editor index', v)
+        },
+      }
+    },
+    mounted: function () {
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" type="text/stylus" scoped>
+  .component-editor {
+    .box-card {
+      background #262626
+      color: #ccc;
+    }
+    .btn-type{
+      max-width 300px
+      position relative
+      img{
+        max-width: 300px
+      }
+      i {
+        position: absolute;
+        right: 0px;
+        top: 0px;
+        color #67C23A
+      }
+      .tip{
+        display: none
+        position absolute
+        width: 100%
+        text-align: center
+        font-size: 10px
+        bottom 0
+        left 0
+        background rgba(0, 0, 0, 0.7)
+        color: #ececec
+        height: 25px;
+        line-height: 25px;
+      }
+      &:hover{
+        .tip{
+          display block
+        }
+      }
+    }
+  }
+</style>
---
+++ src/index.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="component" :style='sty' @click='jump'>
+    {{ctext}}
+  </div>
+</template>
+
+<script>
+  import {VueExtend} from 'godspen-lib'
+
+  export default {
+    mixins: [VueExtend.mixin],
+    name: 'button-a',
+    label: process.env.LABEL,
+    style: process.env.STYLE,
+    props: {
+      text: {
+        type: String,
+        default: '提交',
+        editor: {
+          ignore: true
+        },
+      },
+      url: {
+        type: String,
+        editor: {
+          ignore: true
+        }
+      },
+      color: {
+        type: String,
+        default: 'rgb(255, 255, 255)',
+        editor: {
+          ignore: true
+        }
+      },
+      backgroundColor: {
+        type: String,
+        default: 'rgb(2, 181, 136)',
+        editor: {
+          ignore: true
+        }
+      },
+      radius: {
+        type: [String, Number],
+        default: '3',
+        editor: {
+          ignore: true
+        }
+      },
+      border: {
+        type: String,
+        default: '0',
+        editor: {
+          ignore: true
+        }
+      },
+      borderColor: {
+        type: [String, Number],
+        editor: {
+          ignore: true
+        }
+      }
+    },
+    computed: {
+      ctext () {
+        return this.scopeGet('text')
+      },
+      sty () {
+        return {
+          color: this.color,
+          'background-color': this.backgroundColor,
+          'border-radius': `${this.radius || 0}px`,
+          'border-width': `${this.border || 0}px`,
+          'border-color': this.borderColor
+        }
+      }
+    },
+    methods: {
+      jump () {
+        if (!this.url) return
+        window.location.href = this.scopeGet('url')
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" type="text/stylus" scoped>
+  .component {
+    width: 100%
+    height: 100%
+    display: -webkit-box;
+    -webkit-box-align: center;
+    -webkit-box-pack: center;
+    border-style: solid;
+  }
+</style>
